ebc87d96e9c0aceab9aaac5ba9e101c2dc9ea7e3
peerdavid <david@peer.tirol>
2016-07-12 14:05:30 +0200

    Added license

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # datr
 Download data sets from flickr for given tags.
 
-## Installation
+## Configuration
 1. ```git clone https://github.com/peerdavid/datr/```
 2. Register on flickr and create an application: https://www.flickr.com/services/apps/create/apply/
 3. Copy the new API_Key and API_SECRET.
@@ -12,18 +12,40 @@ API_SECRET = 'YOUR_SECRET'
 ```
 
 ## Execute
-The following command will download 20 images with the tags car && red.<br>
 ``` Bash
-ubuntu:~/Dev/datr$ python datr.py car,red 20
-Removing all files from directory download/
-Downloading images for car,red
-Downloading image Look at that Caddilac
-Downloading image March Chevy 84G IMSA GTP (1984)
-Downloading image Steamer
-[...] 
-  
-Done.  
-Downloaded 20 images. 
+Usage: python datr.py [tags] [num_images] [license]
+Download a data set from flickr for given tags. All tags will be combined with AND.
+  [tags]                A comma-delimited list of tags. Photos with one or more
+                        of the tags listed will be returned. You can exclude 
+                        results that match a term by prepending it
+                        with a - character.
+  [num_images]          Max. num of images to download
+  (optional)[license]   Select the license of the images:
+                           0 - All Rights Reserved
+                           4 - Attribution License
+                           6 - Attribution-NoDerivs License
+                           3 - Attribution-NonCommercial-NoDerivs License
+```
+
+### Example
+The following command will download 10 images of red cars with Public Domain Dedication (CC0) license.<br>
+``` Bash
+ubuntu:~/Dev/datr$ python datr.py car,red 10 9
+Downloading images with tags 'car,red'
+License type set to 9
+
+Downloading image | Title = '20150524_133712LC' | License = 9
+Downloading image | Title = 'Under the Hood' | License = 9
+Downloading image | Title = '1935 Auburn 851 S/C Boattail Speedster' | License = 9
+Downloading image | Title = '1931 Cadillac' | License = 9
+Downloading image | Title = 'Zoom' | License = 9
+Downloading image | Title = 'rusty car 2' | License = 9
+Downloading image | Title = 'rusty car 1' | License = 9
+Downloading image | Title = 'DSC00249' | License = 9
+Downloading image | Title = 'River bank ahead' | License = 9
+
+Done.
+Downloaded 10 images. 
 ```
 
 ## Thanks to
--- datr.py
+++ datr.py
@@ -1,46 +1,65 @@
+#
+# datr
+# Download data sets from flickr
+# See also https://github.com/peerdavid/datr
+#
 import sys
 import os
 import flickr_api as f
 from flickr_api import Walker
 import urllib
 
+
 try :
     search_tags = sys.argv[1]
     num_img = int(sys.argv[2])
     
-    try :
-        access_token = sys.argv[3]
-        f.set_auth_handler(access_token)
-    except IndexError : pass
-    
-    print "Removing all files from directory download/"
+    license = ""
+    if len(sys.argv) == 4:
+        license = sys.argv[3]
+
     os.popen('rm -r download/')
     os.popen('mkdir download')
     
-    print "Downloading images for " + search_tags
+    print "Downloading images with tags '" + search_tags + "'"
+    if license != "":
+        print "License type set to " + license + "\n" 
+    else:
+        print "License type set to any \n"
 
-    # tag_mode (Optional)
-    # Either 'any' for an OR combination of tags, or 'all' for an AND combination. Defaults to 'any' if not specified.
-    w = Walker(f.Photo.search, tags=search_tags, tag_mode='all', extras='url_s', sort='interestingness-desc')
+    w = Walker(f.Photo.search, tags=search_tags, tag_mode='all', extras='url_s', license=license, sort='interestingness-desc')
     i = 0
     for photo in w:
         i += 1
-        if i > num_img:
+        if i >= num_img:
             break
-        print "Downloading image " + photo.title
+        print "Downloading image | Title = '" + photo.title + "' | License = " + photo.license
         urllib.urlretrieve(photo.url_s, "download/image-" + str(i) + ".jpg")
    
-    print "\nDone. \nDownloaded " + str(i) + " images. \n"
+    print "\nDone."
+    print "Downloaded " + str(i) + " images. \n"
+         
          
 except IndexError :
     print ""
-    print "Usage: python datr.py [tags] [num] [access_token_file]"
+    print "Usage: python datr.py [tags] [num_images] [license]"
     print "Download a data set from flickr for given tags. All tags will be combined with AND."
     print ""
     print "  [tags]                A comma-delimited list of tags. Photos with one or more"
     print "                        of the tags listed will be returned. You can exclude " 
     print "                        results that match a term by prepending it"
     print "                        with a - character."
-    print "  [num]                 Max. num of images to download"
-    print "  [access_token_file]   Access token file. Default: flickr_keyss.py"
+    print "  [num_images]          Max. num of images to download"
+    print "  (optional)[license]   Select the license of the images:"
+    print "                           0 - All Rights Reserved"
+    print "                           4 - Attribution License"
+    print "                           6 - Attribution-NoDerivs License"
+    print "                           3 - Attribution-NonCommercial-NoDerivs License"
+    print "                           2 - Attribution-NonCommercial License"
+    print "                           1 - Attribution-NonCommercial-ShareAlike License"
+    print "                           5 - Attribution-ShareAlike License"
+    print "                           7 - No known copyright restrictions"
+    print "                           8 - United States Government Work"
+    print "                           9 - Public Domain Dedication (CC0)"
+    print "                          10 - Public Domain Mark "
     print ""
